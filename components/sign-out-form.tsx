a9887acfc99f3ac9d414577a3e5c354afff2d505
Samuel Corsan <disamprivate@gmail.com>
2024-12-14 18:07:21 +0100

    sign out form

--- components/sign-out-form.tsx
+++ components/sign-out-form.tsx
@@ -1,25 +1,26 @@
-import Form from 'next/form';
+"use client";
 
-import { signOut } from '@/app/(auth)/auth';
+import { signOut } from "@/app/(auth)/auth";
 
 export const SignOutForm = () => {
-  return (
-    <Form
-      className="w-full"
-      action={async () => {
-        'use server';
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
 
-        await signOut({
-          redirectTo: '/',
-        });
-      }}
-    >
+    ("use server"); // Marcar que el bloque se ejecuta en el servidor (si aplica)
+
+    await signOut({
+      redirectTo: "/",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full">
       <button
         type="submit"
         className="w-full text-left px-1 py-0.5 text-red-500"
       >
         Sign out
       </button>
-    </Form>
+    </form>
   );
 };
